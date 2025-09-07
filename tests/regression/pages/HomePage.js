53b98ef01067c2d9d82246f0a28552c4511ba358
Sazid Ahmed Nassir <sazidahmednassir@gmail.com>
2025-09-07 15:33:18 +0600

    updated homepages file

--- tests/regression/pages/HomePage.js
+++ tests/regression/pages/HomePage.js
@@ -1,7 +1,7 @@
 // Function-style POM: expose locator getters by page
 const HomePage = {
   heroCarousel: (page) => page.locator('.wdt-cus-home2-slider'),
-  heroHeading: (page) => page.locator('.wdt-cus-home2-slider-heading .wdt-heading-title23'),
+  heroHeading: (page) => page.locator('.wdt-cus-home2-slider-heading .wdt-heading-title'),
   heroBulletItems: (page) => page.locator('.wdt-cus-home2-slider-list .elementor-icon-list-item'),
   heroCTA: (page) => page.locator('.wdt-cus-home2-slider-button a.wdt-button'),
 };
